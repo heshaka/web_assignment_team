cc0d610c6eabc4320fdea1c1aa0f0e274ddd2561
heshaka <68396977+heshaka@users.noreply.github.com>
2020-08-27 11:18:40 +0530

    made php for contact

--- contactUs1.php
+++ contactUs1.php
@@ -1,27 +1,51 @@
 
 <?php
+$con=mysqli_connect("localhost","root",""); 
+if($con===false)   				
+{
+ echo "Not connected to server";
+}
+
+
+echo"connected successfully";
+
+if(!mysqli_select_db($con, 'hotelian'))
+{
+ echo 'Database invalid';
+
+}
+else{
+	echo"<br>connected to hotelian<br>";
+}
 
 $firstname=$_REQUEST['firstname'];
 $lastname=$_REQUEST['lastname'];
-$telnumber=$_REQUEST['telephone number'];
+$telnumber=$_REQUEST['telephonenumber'];
 $address=$_REQUEST['address'];
 $city=$_REQUEST['city'];
-$detail=$_REQUEST['Details'];
-$con=mysqli_connect("localhost","root","","hotelian");
-if($link===false)
-{
-	die("ERROR:could not connect.".mysqli_connect_error());
-}
-$query="INSERT INTO hotelcontacts(Fname,Lname,Email,Number,City) VALUES ('$firstname','$lastname','$telnumber','$address','$city','$detail')";
+
+
+$query="INSERT INTO hotelcontacts(Fname,Lname,email,number,city) VALUES ('$firstname','$lastname','$telnumber','$address','$city')";
 if(mysqli_query($con,$query))
 {
 	echo"recorded data successfully";
+
 }
 else{
 	echo"ERROR:could not able to execute $query".mysqli_error($con);
 }
 mysqli_close($con);
+
 ?>
+<html>
+<head>
+	<body>
+	<input type="button" value="home" href="home.html">
+	</body>
+</head>
+</html>
+
+
 
 
 
